13965e2809ddbea56aad71a6d78a65a0346a8c5d
Johann Severin <johann.bock.severin@hotmail.com>
2023-09-28 12:09:23 +0200

    Combined perfect and imperfect measurements

--- Simulations/readout_simulations/qfunc_trajectories_plot.py
+++ Simulations/readout_simulations/qfunc_trajectories_plot.py
@@ -100,8 +100,8 @@ measurements_excited = np.squeeze(sme_results.measurements[1]) / np.sqrt(
 )
 
 plt.rcParams["axes.titlesize"] = 16
-plt.rcParams["figure.figsize"] = (18, 10)
-fig, ax = plt.subplots(ncols=3, nrows=2, sharey="row")
+plt.rcParams["figure.figsize"] = (18, 16)
+fig, ax = plt.subplots(ncols=3, nrows=3, sharey="row")
 
 for i in range(Qs_ground.shape[0]):
     ax[0, i].contour(x, y, Qs_ground[i], cmap=cmap_0)
@@ -111,6 +111,10 @@ for i in range(Qs_ground.shape[0]):
     ax[1, i].contour(
         x, y, convolve2d(Qs_excited[i], gaussian, mode="same"), cmap=cmap_1
     )
+
+ax[0, 2].plot([], [], color="C0", label="Ground")
+ax[0, 2].plot([], [], color="C1", label="Excited")
+ax[0, 2].legend(loc="upper right")
 
 
 for i, time in enumerate([0, 20, 40]):
@@ -136,14 +140,87 @@ for i, time in enumerate([0, 20, 40]):
 
     ax[1, i].set(
         xlabel="I (photons)",
-        title=f"Convolved QFunc and 10 ns Record",
+        title=f"10 ns Record - $\eta = 10 \%$",
     )
 
 for i in range(2):
     ax[i, 0].set(ylabel="Q (photons)")
 
 
-fig.suptitle(f"Readout Q Functions and 10 ns Records - $\eta = 10\%$")
+# At 0, 200 and 400 ns
+rhos_ground = lindblad_results.states[0][[0, 20, 40]]
+rhos_excited = lindblad_results.states[1][[0, 20, 40]]
+
+interval = 20
+resolution = 200
+
+from matplotlib.colors import LinearSegmentedColormap
+
+cmap_0 = LinearSegmentedColormap.from_list("cmap_0", ["white", "C0"])
+cmap_1 = LinearSegmentedColormap.from_list("cmap_1", ["white", "C1"])
+
+
+Qs_ground = get_Q_func(rhos_ground, interval, resolution, lindblad_results)
+Qs_excited = get_Q_func(rhos_excited, interval, resolution, lindblad_results)
+
+
+x = np.linspace(-interval, interval, resolution)
+y = np.linspace(-interval, interval, resolution)
+
+
+X, Y = np.meshgrid(x, y)
+gaussian = multivariate_normal(mean=[0, 0], cov=[[20, 0], [0, 20]]).pdf(
+    np.dstack([X, Y])
+)
+
+sme_results = pickle.load(
+    open(
+        "/mnt/c/Users/johan/OneDrive/Skrivebord/Master_Thesis/Simulations/readout_simulations/data/qfunc_trajectories_sme_perfect.pkl",
+        "rb",
+    )
+)
+
+measurements_ground = np.squeeze(sme_results.measurements[0]) / np.sqrt(
+    config["kappa"] * timescale * 2
+)
+measurements_excited = np.squeeze(sme_results.measurements[1]) / np.sqrt(
+    config["kappa"] * timescale * 2
+)
+
+
+for i in range(Qs_ground.shape[0]):
+    # ax[0, i].contour(x, y, Qs_ground[i], cmap=cmap_0, label="Ground")
+    # ax[0, i].contour(x, y, Qs_excited[i], cmap=cmap_1, label="Excited")
+
+    ax[2, i].contour(x, y, convolve2d(Qs_ground[i], gaussian, mode="same"), cmap=cmap_0)
+    ax[2, i].contour(
+        x, y, convolve2d(Qs_excited[i], gaussian, mode="same"), cmap=cmap_1
+    )
+
+ax[0, 2].legend(loc="upper right")
+
+for i, time in enumerate([0, 20, 40]):
+    ax[2, i].scatter(
+        measurements_ground[time, :, 0],
+        measurements_ground[time, :, 1],
+        color="C0",
+        alpha=0.5,
+    )
+
+    ax[2, i].scatter(
+        measurements_excited[time, :, 0],
+        measurements_excited[time, :, 1],
+        color="C1",
+        alpha=0.5,
+    )
+
+    ax[2, i].set(title=f"With $\eta = 100\%$", xlabel="I (photons)")
+
+for i in range(3):
+    ax[i, 0].set(ylabel="Q (photons)")
+
+fig.suptitle(f"Readout Q Functions and 10 ns Records")
 fig.tight_layout()
 
-fig.savefig("figures/qfunc_trajectories_perfect.pdf")
+# fig.savefig("figures/qfunc_trajectories.pdf")
+#
